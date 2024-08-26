e6326ecca9591dea03ed1425ca345017aab9bab2
mhwelander <welander@prisma.io>
2024-08-26 21:08:34 +0200

    feat: Adde a table of contents integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,25 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import customToc from "astro-custom-toc";
+
+const tocTemplate = (html) => {
+  return `
+<aside class="toc">
+  <nav>
+      ${html}
+  </nav>
+</aside>`.trim();
+};
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mhwelander.net/',
-	integrations: [mdx(), sitemap()],
+  site: 'https://mhwelander.net/',
+  integrations: [customToc(
+    {
+      template: tocTemplate,
+      maxDepth: 3
+    }
+  ), mdx(), sitemap()]
 });
+
